2dbbf6090c48ba1c4e091a65f1146491ad7d2bb7
Olamide <olowofesovictor@gmail.com>
2020-09-28 05:36:08 +0100

    Trying out authentication, working...

---
+++ application/mod_auth/controllers.py
@@ -0,0 +1,72 @@
+# Import flask dependencies
+from flask import Blueprint, request, render_template,flash, g, session, redirect, url_for, jsonify, abort
+
+# Import password / encryption helper tools
+from werkzeug.security import check_password_hash
+
+# Import the database object from the main app module
+from application import db, app
+
+
+# Import flask_login for auth
+import flask_login
+
+# Import module models (i.e. User)
+from application.mod_auth.models import User
+
+# Login manger setup
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(username):
+    return User.query.filter_by(username=username).first()
+
+
+# Define the blueprint: 'auth', set its url prefix: app.url/auth
+mod_auth = Blueprint('mod_auth', __name__, url_prefix='/auth', template_folder='templates/mod_auth')
+login_manager.login_view = "mod_auth.login"
+
+
+@mod_auth.route('/index/', methods=['GET'])
+@flask_login.login_required
+def index():
+    return render_template('index.html', a=a)
+
+
+# Set the route and accepted methods
+@mod_auth.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    user = User(
+        username='olamide',
+        email='ola@uni.com', 
+        password='user1234',
+        )
+    db.session.add(user)
+    db.session.commit()
+    # return "Hello You Made it!!!"
+    li = []
+    for i in User.query.all():
+        li.append(i.user_id)
+    return jsonify(user=li)
+
+@mod_auth.route('/login/', methods=['GET', 'POST'])
+def login():
+    user = User.query.filter_by(username='olamide').first()
+    flask_login.login_user(user)
+    # user.authenticated = True
+    next = request.args.get('next')
+    
+    if not is_safe_url(next):
+        return abort(400)
+    
+    return redirect(next or flask.url_for('index'))
+
+
+
+
+def is_safe_url(target):
+    from urllib.parse import urlparse, urljoin
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
---
+++ application/mod_auth/models.py
@@ -0,0 +1,50 @@
+import application
+
+from application import db 
+from datetime import datetime
+import uuid
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Define a User model
+class User(db.Model):
+
+    __tablename__ = 'user'
+
+    # User Name
+    user_id    = db.Column(db.String(40),  nullable=False, primary_key=True)
+    username   = db.Column(db.String(15), nullable=False)
+    first_name = db.Column(db.String(30), nullable=True)
+    last_name = db.Column(db.String(30), nullable=True)
+    email    = db.Column(db.String(128),  nullable=False)
+    password = db.Column(db.String(192),  nullable=False)
+    role     = db.Column(db.String(10), nullable=False)
+    # authenticated = db.Column(db.Boolean, default=False)
+    created_on = db.Column(db.DateTime, nullable=False)
+
+
+    # New instance instantiation procedure
+    def __init__(self, username, email, password, first_name='', last_name='', role='Student'):
+
+        self.user_id        = str(uuid.uuid4()) 
+        self.username       = username
+        self.first_name     = first_name
+        self.last_name      = last_name
+        self.email          = email
+        self.password       = generate_password_hash(password)
+        self.role           = role
+        self.created_on     = datetime.utcnow()
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)   
+
+    def get_id(self):
+        return self.username
+
+    def is_active(self):
+        return True
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_anonymous(self):
+        return False
